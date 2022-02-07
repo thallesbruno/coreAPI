dc3cbf2e1b7e69bd3701748f1e10d74cb9037166
Thalles Santos <thallesbgns@gmail.com>
2022-02-07 14:51:48 -0300

    Finishing the course

--- API/webAPI/Startup.cs
+++ API/webAPI/Startup.cs
@@ -1,14 +1,11 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Newtonsoft.Json.Serialization;
+using System.IO;
 
 namespace webAPI
 {
@@ -24,12 +21,29 @@ namespace webAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //Enable CORS
+            services.AddCors(c =>
+            {
+                c.AddPolicy("AllowOrigin", options => options.AllowAnyOrigin().AllowAnyMethod()
+                 .AllowAnyHeader());
+            });
+
+            //JSON Serializer
+            services.AddControllersWithViews()
+                .AddNewtonsoftJson(options =>
+                options.SerializerSettings.ReferenceLoopHandling = Newtonsoft
+                .Json.ReferenceLoopHandling.Ignore)
+                .AddNewtonsoftJson(options => options.SerializerSettings.ContractResolver
+                = new DefaultContractResolver());
+
             services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -43,6 +57,14 @@ namespace webAPI
             {
                 endpoints.MapControllers();
             });
+
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider(
+                    Path.Combine(Directory.GetCurrentDirectory(),
+                    "Photos")),
+                RequestPath = "/Photos"
+            }); ;
         }
     }
 }
---
+++ UI/angular10/src/app/department/show-dep/show-dep.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit } from '@angular/core';
+import { SharedService } from "src/app/shared.service";
+
+@Component({
+  selector: 'app-show-dep',
+  templateUrl: './show-dep.component.html',
+  styleUrls: ['./show-dep.component.css']
+})
+export class ShowDepComponent implements OnInit {
+
+  constructor(private service:SharedService) { }
+
+  DepartmentList:any=[];
+
+  ModalTitle:string;  
+  ActivateAddEditDepComp:boolean=false;
+  dep:any;
+
+  ngOnInit(): void {
+    this.refreshDepList();
+  }
+
+  addClick(){
+    this.dep={
+      DepartmentId:0,
+      DepartmentName:""
+    }
+    this.ModalTitle="Add Department";
+    this.ActivateAddEditDepComp=true;
+  }
+
+  editClick(item){
+    this.dep=item;
+    this.ModalTitle="Edit Department";
+    this.ActivateAddEditDepComp=true;
+  }
+
+  deleteClick(item){
+    if (confirm('Are you sure?')) {
+      this.service.deleteDepartment(item.DepartmentId).subscribe(data=>{
+        alert(data.toString());
+        this.refreshDepList();
+      });
+    }
+  }
+
+  closeClick(){
+    this.ActivateAddEditDepComp=false;
+    this.refreshDepList();
+  }
+
+  refreshDepList() {
+    this.service.getDepList().subscribe(data=>{
+      this.DepartmentList=data;
+    });
+  }
+
+}
